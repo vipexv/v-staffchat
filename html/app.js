ab9ca9bb18295cc26e820dbef076f6b781b5475a
vipex <101529155+vipexv@users.noreply.github.com>
2023-05-13 12:55:25 +0200

    Update app.js

--- html/app.js
+++ html/app.js
@@ -27,14 +27,9 @@ $(document).ready(function () {
 
   function dragMoveListener(event) {
     var target = event.target
-    // keep the dragged position in the data-x/data-y attributes
     var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
     var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
-
-    // translate the element
     target.style.transform = 'translate(' + x + 'px, ' + y + 'px)'
-
-    // update the posiion attributes
     target.setAttribute('data-x', x)
     target.setAttribute('data-y', y)
   }
@@ -189,7 +184,6 @@ window.addEventListener('message', function (event) {
       notify(`${sourcename} Sent a New Image!`)
       addImage(sourcename, imagelink);
     }
-    // addImage(sourcename, imagelink);
     messageInput.value = '';
   } 
 });
